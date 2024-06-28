33a2015f3bc81ee1fb354f9e23d66a88417b16f3
da5ater <vincentvan205ater@gmail.com>
2024-06-28 04:15:36 +0300

    parsed hrml lines

--- Str/hrml.cpp
+++ Str/hrml.cpp
@@ -16,7 +16,7 @@ int main()
     // int N /*number of lines in the HRML program*/, Q /*number of queries*/;
     // cin >> N >> Q;
     // string line;
-    // vector<string> hml_container;
+    // vector<string> hrml_container;
     // for (int i = 0; i < N; i++)
     // {
     //     getline(cin, line);
@@ -25,7 +25,7 @@ int main()
     //     }
     //     else
     //     {
-    //         hml_container.push_back(line);
+    //         hrml_container.push_back(line);
     //     }
     // }
 
@@ -38,20 +38,20 @@ int main()
     // };
 
     // ---------------------------------------------------------------
-    vector<string> hml_container;
+    vector<string> hrml_container;
 
     // Option 1: Pre-defined lines (assuming you have the content ready)
-    hml_container.push_back("<tag1 value = \"HelloWorld\">");
-    hml_container.push_back("<tag2 name = \"Name1\">");
-    hml_container.push_back("</tag2>");
-    hml_container.push_back("</tag1>");
+    hrml_container.push_back("<tag1 value = \"HelloWorld\">");
+    hrml_container.push_back("<tag2 name = \"Name1\">");
+    hrml_container.push_back("</tag2>");
+    hrml_container.push_back("</tag1>");
 
     vector<string> q_container;
 
     // Define the input strings (replace with your actual input method)
-    string input1 = "<tag1 tag1.tag2~name";
-    string input2 = "tag1~name";
-    string input3 = "tag1~value";
+    string input1 = "<tag1 tag1.tag2~name>";
+    string input2 = "tag1~name>";
+    string input3 = "tag1~value>";
 
     // Add each input string to the vector
     q_container.push_back(input1);
@@ -59,33 +59,48 @@ int main()
     q_container.push_back(input3);
 
     //----------------------------------------------------------------
+    map<string, string> attributes; // store tag attribute pairs
+    vector<string> tag_stack;       // keep track of nested tags
 
-    stack<string> nested_tags;
-    map<string, string> path_attribute;
-
-    for (size_t i = 0; i < hml_container.size(); i++)
+    for (int i = 0; i < hrml_container.size(); i++) // do not forgent n
     {
-        if (hml_container[i].find('/') != string::npos) // closing tag
+        if (hrml_container[i].substr(0, 2) == "</") // closing tag
         {
+            /* code */
         }
-        else
-        { // opening tag
-            size_t space_pos = hml_container[i].find(" ");
-            string tag_name = hml_container[i].substr(1, space_pos - 1);
+        else // openenig tag
+        {
+            string curated_tag = hrml_container[i].substr(1, hrml_container[i].size() - 2);
 
-            regex attr_regex(R"((\w+)=["']([^"']+)["'])");
-            smatch match;
-            string tag = hml_container[i];
-            while (regex_search(tag, match, attr_regex))
+            stringstream ss(curated_tag);
+
+            string tag_name, key, equal_sign, value;
+            ss >> tag_name >> key >> equal_sign >> value;
+            value = value.substr(1, value.size() - 2); // remove quotes
+
+            string full_tag_name = "";
+            if (!tag_stack.empty()) // not empty
             {
-                path_attribute[match[1].str()] = match[2].str();
-                tag = match.suffix().str();
+                full_tag_name = tag_stack.back() + "." + tag_name;
             }
+            else
+            { // empty
+                full_tag_name = tag_name;
+            }
+            tag_stack.push_back(full_tag_name); // to access the tag
+
+            attributes[full_tag_name + '~' + key] = value;
         }
     }
-
-    for (auto &pair : path_attribute)
+    for (const auto &pair : attributes)
     {
-        cout << pair.first << "::" << pair.second << endl;
+        std::cout << pair.first << " :: " << pair.second << '\n';
+    }
+
+    cout << "------------\n";
+
+    for (auto &&i : tag_stack)
+    {
+        cout << i << endl;
     }
 }
